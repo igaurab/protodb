b0468127aa4ff1924aa30a6b3b4de755b2fbe149
igaurab <dev@igaurab.com>
2024-06-04 17:05:03 +0545

    Read input from terminal and send it to server.

--- client/client.go
+++ client/client.go
@@ -1,24 +1,41 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"os"
 )
 
 func main() {
+	SERVER_URL := "localhost"
+	SERVER_PORT := "8080"
+	SERVER_ADDR := SERVER_URL + ":" + SERVER_PORT
+	conn, err := net.Dial("tcp", SERVER_ADDR)
 
-	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	defer conn.Close()
 
-	if err != nil {
-		fmt.Println(err)
-		return
+	fmt.Println("Connected to server at ", SERVER_ADDR)
+
+	for {
+		fmt.Print("# ")
+		reader := bufio.NewReader(os.Stdin)
+		str, _, _ := reader.ReadLine()
+		fmt.Println("Client command: ", string(str))
+		_, err = conn.Write(str)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 	}
 
-	var str = "this is the good times that has been one of the most intresting times of the year so that i have to goto the gym at the very least of the time and i don't want to do that at the meoment"
-	_, err = conn.Write([]byte(str))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+}
+
+func requestServer(conn net.Conn) {
+
 }
